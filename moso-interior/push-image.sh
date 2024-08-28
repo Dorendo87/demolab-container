1efd5db85132fd86b19e13a35af36fe05463c3d2
Dorendo87 <shahril.sukri87@gmail.com>
2024-08-28 05:57:37 +0000

    2nd commit

--- moso-interior/push-image.sh
+++ moso-interior/push-image.sh
@@ -10,4 +10,4 @@ docker login --username $DOCKER_HUB_USERNAME --password $DOCKER_HUB_PASSWORD
 docker tag $IMAGE_TAG $DOCKER_HUB_USERNAME/$DOCKER_HUB_REPO_NAME
 
 # Push the image to Docker Hub repository
-docker push $DOCKER_HUB_USERNAME/$DOCKER_HUB_REPO_NAMEHere, we’ll create a buildspec file which is in YAML format. It is a collection of build commands and settings that CodeBuild uses to execute our Build jobs. To know more about it and how to write it, visit this link; https://docs.aws.amazon.com/codebuild/latest/userguide/build-spec-ref.html
+docker push $DOCKER_HUB_USERNAME/$DOCKER_HUB_REPO_NAME #Here, we’ll create a buildspec file which is in YAML format. It is a collection of build commands and settings that CodeBuild uses to execute our Build jobs. To know more about it and how to write it, visit this link; https://docs.aws.amazon.com/codebuild/latest/userguide/build-spec-ref.html
